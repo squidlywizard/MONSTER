15ec47401cb0f65d916e50e7f584264ba89dcc12
squidlywizard <ztdenna@hotmail.com>
2018-09-12 08:09:09 -0600

    we put in mutators and accessors

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -21,6 +21,7 @@ public class MarshmallowMonster
 	}
 	
 	public MarshmallowMonster(String name, double legCount, int eyeCount, boolean hasNoses, int armCount)//parameters
+
 	{
 		this.name = name;
 		this.legCount = legCount;
@@ -28,7 +29,56 @@ public class MarshmallowMonster
 		this.hasNoses = hasNoses;
 		this.armCount = armCount;
 	}
+	
+	//Getters Acessors
+	
+	public String getName()
+
+	{
+		
+		return name;// this closes the method btw
+		
+	}
+	public double getLegCount()
+	{
+		return legCount;
+	}
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}	
+	public boolean getHasNoses()
+	{
+		return hasNoses;
+	}
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	//Setters change or set the value use if you need to export variables
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setLegCount(double legs)
+	{
+	    this.legCount = legs;	
+	}
+	public void setEyeCount(int eyes)
+	{
+	this.eyeCount = eyes;	
+	}
+	public void sethasNoses(boolean nose)
+	{
+		this.hasNoses = nose;
+	}
+	public void setArmCount(int arms)
+	{
+		this.armCount = arms;
+	}
 	public String toString()
+    
+    
 	{
 		String descripption = "My mosters name is " + name + " it has " 
 				+ legCount + " legs"
